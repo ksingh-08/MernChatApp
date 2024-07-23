217e87e2465cc154b1477eec9262c39d50a51c7e
ksingh-08 <mailtoksingh08@gmail.com>
2024-07-23 23:13:45 +0530

    latest

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 import  { UserContextProvider } from './components/UserContext'
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './components/Routes'
+
 function App() {
  axios.defaults.baseURL='https://mernchatapp-backend-lt9f.onrender.com';
  axios.defaults.withCredentials=true;
